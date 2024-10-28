758587e3876f8c4788b70562ced77ff08cdbbefe
olofbaage <olofbaage@gmail.com>
2024-10-28 19:03:51 +0100

    add function show/hide submenu workflows

--- monogramcc/assets/js/app.js
+++ monogramcc/assets/js/app.js
@@ -1,1 +1,11 @@
-console.log("hello, world");
+// all elements
+const btnWorkflows = document.getElementById("btn-workflows");
+const list__submenuWorkflows = document.getElementById("list__submenuW-workflows");
+
+// functions
+function openSubmenuWorkflows() {
+    list__submenuWorkflows.classList.toggle("ul__li__ul--invisbile");
+}
+
+// event listeners
+btnWorkflows.addEventListener('click', openSubmenuWorkflows, false);
