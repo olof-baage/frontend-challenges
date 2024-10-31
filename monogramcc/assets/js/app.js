f84d86d6d1d28fe2bb2ded17421bc3ae78d1ba26
olofbaage <olofbaage@gmail.com>
2024-10-31 21:17:49 +0100

    workign on fixed menu

--- monogramcc/assets/js/app.js
+++ monogramcc/assets/js/app.js
@@ -1,11 +1,18 @@
 // all elements
 const btnWorkflows = document.getElementById("btn-workflows");
-const list__submenuWorkflows = document.getElementById("list__submenuW-workflows");
+const btnCurrency = document.getElementById("btn-currency");
+const list__submenuWorkflows = document.getElementById("list__submenu-workflows");
+const list__submenuCurrency = document.getElementById("list__submenu-currency");
 
 // functions
 function openSubmenuWorkflows() {
     list__submenuWorkflows.classList.toggle("ul__li__ul--invisbile");
 }
 
+function openSubmenuCurrency() {
+    list__submenuCurrency.classList.toggle("ul__li__ul--invisbile");
+}
+
 // event listeners
 btnWorkflows.addEventListener('click', openSubmenuWorkflows, false);
+btnCurrency.addEventListener('click', openSubmenuCurrency, false);
